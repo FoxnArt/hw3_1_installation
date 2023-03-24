c4a9bf6c91047762129154c3132e6bf781bd0328
FoxnArt <andrei_va80@mail.ru>
2023-03-24 11:03:56 +0300

    Modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,68 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 
 public class Main {
-    public static StringBuilder stringBuilder;
 
     public static void main(String[] args) {
-        createDir(dir1, "D://Games/src");
-//         dir1, "D://Games/src"
-//         dir2, "D://Games/res"
-//         dir3, "D://Games/savegames"
-//         dir4, "D://Games/temp"
-//         dir5, "D://Games/src/main"
-//         dir6, "D://Games/src/test"
-//         dir7, "D://Games/res/drawables"
-//         dir8, "D://Games/res/vectors"
-//         dir9, "D://Games/res/icons"
+        String homeDir = "D://Games";
+        String[] dirPathArr = {
+                "/src",
+                "/res",
+                "/savegames",
+                "/temp",
+                "/src/main",
+                "/src/test",
+                "/res/drawables",
+                "/res/vectors",
+                "/res/icons"
+        };
+        String[] filePathArr = {
+                "/src/main/Main.java",
+                "/src/main/Utils.java",
+                "/temp/temp.txt"
+        };
 
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String value : dirPathArr) {
+            stringBuilder.append(createMyDir(homeDir + value));
+        }
+        for (String value : filePathArr) {
+            stringBuilder.append(createMyFile(homeDir + value));
+        }
 
-
+        String log = stringBuilder.toString();
+        String logPath = homeDir + "/temp/temp.txt";
+        writeLog(logPath, log);
     }
 
-    public static String createDir (String name, String pathname) {
-        File name = new File(pathname);
-        if (dir1.mkdir())
-                stringBuilder.append("Папка D://Games/src создана\n");
-            else
-                stringBuilder.append("Папка D://Games/src не создана\n");
+    public static String createMyDir(String dirPath) {
+        File dirName = new File(dirPath);
+        if (dirName.mkdir()) {
+            return (new Date() + " : " + dirPath + " : folder created successfully\n");
+        } else {
+            return (new Date() + " : " + dirPath + " : folder not created\n");
+        }
+    }
 
+    public static String createMyFile(String filePath) {
+        File fileName = new File(filePath);
+        try {
+            if (fileName.createNewFile())
+                return (new Date() + " : " + filePath + " : file created successfully\n");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return (new Date() + " : " + filePath + " : file not created\n");
+    }
+
+    public static void writeLog(String logPath, String log) {
+        try (FileWriter writer = new FileWriter(logPath)) {
+            writer.write(log);
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
